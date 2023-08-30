e0ae3b0aa7db1ac4e550248bf517e3a318b2ca53
Alexander Oviedo Fadul <45949794+bladealex9848@users.noreply.github.com>
2023-08-30 03:18:56 -0500

    commit inicial

---
+++ README.md
@@ -0,0 +1,71 @@
+# PDFMaster: Tu asistente de documentos PDF
+
+## Descripción
+
+PDFMaster es una herramienta completa para gestionar documentos PDF. Permite realizar diversas tareas como convertir PDF a Word, generar resúmenes, realizar preguntas y obtener respuestas específicas de un documento, y muchas otras funcionalidades que se están desarrollando.
+
+## Introducción
+
+Con PDFMaster, en menos de 50 líneas de código, aprenderás a aprovechar todo el potencial de chatGPT y convertir tus documentos en conversaciones interactivas. No más lecturas aburridas o búsquedas tediosas, ahora podrás hacer preguntas directamente a tus documentos y obtener respuestas de chatGPT. Para desarrollar esta aplicación necesitaremos:
+
+- OpenAI ChatGPT API
+- Streamlit
+
+## ¿Cómo funciona?
+
+1. Divide documento en fragmentos (o chunks)
+2. Crea los embeddings de los fragmentos de texto
+3. Guarda los fragmentos y los embeddings en una base de conocimiento
+4. Busca los fragmentos más similares a la pregunta del usuario gracias a los embeddings.
+5. Pasa los fragmentos más similares junto a la pregunta a chatGPT que genera la respuesta
+
+## Funcionalidades
+
+- **Realizar preguntas**: Permite hacer preguntas específicas sobre el contenido de un documento PDF y obtener respuestas precisas.
+- **Convertir a Word**: Convierte documentos PDF en documentos Word (.docx).
+- **Generar resumen**: Genera un resumen del contenido de un documento PDF.
+
+## Funcionalidades Futuras
+
+- Comprimir PDF
+- Aplicar OCR (Reconocimiento óptico de caracteres) a un PDF
+- ... y más.
+
+## Instalación
+
+1. Asegúrate de tener Python 3.8 o superior instalado en tu máquina.
+2. Clona este repositorio usando `git clone https://github.com/bladealex9848`
+3. Navega al directorio del proyecto `cd PDFMaster`
+4. Instala las dependencias necesarias usando `pip install -r requirements.txt`
+5. Ejecuta `streamlit run app.py` para iniciar la aplicación.
+6. Obtén una clave API de OpenAI para usar su API ChatGPT.
+
+## Uso
+
+1. **Realizar preguntas**
+
+- Carga un documento PDF.
+- Selecciona la opción 'Realizar preguntas' en el menú de la izquierda.
+- Escribe tu pregunta en el cuadro de texto.
+- Presiona 'Enter' o haz clic fuera del cuadro de texto para obtener la respuesta.
+
+2. **Convertir a Word**
+
+- Carga un documento PDF.
+- Selecciona la opción 'Convertir a Word' en el menú de la izquierda.
+- Haz clic en 'Convertir' y espera a que la conversión se complete.
+- Haz clic en 'Descargar archivo Word' para descargar el archivo convertido.
+
+3. **Generar resumen**
+
+- Carga un documento PDF.
+- Selecciona la opción 'Generar resumen' en el menú de la izquierda.
+- El resumen se generará automáticamente y se mostrará en pantalla.
+
+## Contribuciones
+
+Si deseas contribuir a este proyecto, por favor, realiza un fork de este repositorio, crea una nueva rama, realiza tus cambios y envía un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo `LICENSE` para obtener más detalles.
---
+++ app.py
@@ -0,0 +1,149 @@
+import os
+import tempfile
+import streamlit as st
+from PyPDF2 import PdfReader
+from pdf2docx import Converter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+import io
+from transformers import BertTokenizer
+
+st.set_page_config('PDFMaster')
+st.title("PDFMaster: Tu asistente de documentos PDF")
+
+# Cargar API_KEY desde una variable de entorno, un archivo o el input del usuario
+API_KEY = os.environ.get('API_KEY')
+if API_KEY is None:
+    try:
+        with open('api_key.txt', 'r') as f:
+            API_KEY = f.read().strip()
+    except FileNotFoundError:
+        with open('api_key.txt', 'w') as f:
+            f.write('tu_clave_de_api')
+        API_KEY = st.text_input('OpenAI API Key', type='password')
+
+pdf_obj = st.file_uploader("Carga tu documento", type="pdf")
+
+
+@st.cache_resource
+def create_embeddings(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=100,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    return knowledge_base, text
+
+
+def convert_pdf_to_word(pdf_obj):
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(pdf_obj.read())
+    tfile.close()  # Asegúrate de cerrar el archivo
+
+    # Obtener el nombre del archivo sin la extensión
+    filename = os.path.splitext(pdf_obj.name)[0]
+    word_filename = f"{tempfile.gettempdir()}/{filename}.docx"
+
+    cv = Converter(tfile.name)
+    cv.convert(word_filename, start=0, end=None)
+    cv.close()
+
+    os.unlink(tfile.name)  # Elimina el archivo
+
+    with open(word_filename, 'rb') as f:
+        word_file = io.BytesIO(f.read())
+
+    os.unlink(word_filename)  # Elimina el archivo
+
+    return word_file
+
+
+def generate_summary(text):
+    import openai
+
+    openai.api_key = API_KEY
+
+    # Cargar un tokenizador pre-entrenado
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Dividir el texto en tokens
+    tokens = tokenizer.tokenize(text)
+    num_tokens = len(tokens)
+    chunk_size = 4000
+    segments = [tokens[i:i + chunk_size]
+                for i in range(0, num_tokens, chunk_size)]
+    summary = ""
+
+    for segment in segments:
+        segment_text = tokenizer.convert_tokens_to_string(segment)
+
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"Por favor, proporciona un resumen del siguiente texto:\n\n{segment_text}"}
+        ]
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=300,
+            temperature=0.5,
+        )
+
+        summary += response['choices'][0]['message']['content'].strip()
+
+    return summary
+
+
+if pdf_obj:
+    st.sidebar.header('Opciones')
+    options = [
+        'Realizar preguntas',
+        'Convertir a Word',
+        'Generar resumen',
+        # 'Comprimir PDF',
+        # 'Aplicar OCR',
+    ]
+    selected_option = st.sidebar.selectbox(
+        "¿Qué deseas hacer con el PDF?", options)
+
+    if selected_option == 'Realizar preguntas':
+        st.header("Realizar preguntas")
+        knowledge_base, _ = create_embeddings(pdf_obj)
+        user_question = st.text_input("Haz una pregunta sobre tu PDF:")
+
+        if user_question:
+            os.environ["OPENAI_API_KEY"] = API_KEY
+            docs = knowledge_base.similarity_search(user_question, 3)
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+            chain = load_qa_chain(llm, chain_type="stuff")
+            respuesta = chain.run(input_documents=docs, question=user_question)
+            st.write(respuesta)
+
+    elif selected_option == 'Convertir a Word':
+        st.header("Convertir a Word")
+        if st.button("Convertir"):
+            word_file = convert_pdf_to_word(pdf_obj)
+            # Obtener el nombre del archivo sin la extensión
+            filename = os.path.splitext(pdf_obj.name)[0]
+            st.download_button('Descargar archivo Word',
+                               word_file, f'{filename}.docx')
+
+    elif selected_option == 'Generar resumen':
+        st.header("Generar resumen")
+        _, text = create_embeddings(pdf_obj)
+        resumen = generate_summary(text)
+        st.write(resumen)
