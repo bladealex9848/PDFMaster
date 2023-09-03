3be81453d2a9728a713beaf35e2f53c4f89f3929
Alexander Oviedo Fadul <45949794+bladealex9848@users.noreply.github.com>
2023-09-02 22:40:21 -0500

    Adición del selector de idioma

--- app_streamlit.py
+++ app_streamlit.py
@@ -6,45 +6,44 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-from transformers import BertTokenizer
 from langdetect import detect
 
-# Configuración de Streamlit
+# Configuración de Streamlit / Streamlit Configuration
 st.set_page_config('PDFMaster')
 st.title("PDFMaster: Tu asistente de documentos PDF")
 
-# Cargar API Key
-# Intenta cargar la API Key desde st.secrets
+# Cargar API Key / Load API Key
+# Intenta cargar la API Key desde st.secrets / Try to load API Key from st.secrets
 API_KEY = st.secrets.get('API_KEY')
 
-# Si la API Key no está en st.secrets, pídela al usuario
+# Si la API Key no está en st.secrets, pídela al usuario / If API Key is not in st.secrets, ask the user
 if not API_KEY:
     API_KEY = st.text_input('OpenAI API Key', type='password')
 
-# Si no se ha proporcionado la API Key, no permitas que el usuario haga nada más
+# Si no se ha proporcionado la API Key, no permitas que el usuario haga nada más / If API Key is not provided, do not allow the user to do anything else
 if not API_KEY:
     st.stop()
 
-# Cargar PDF
+# Cargar PDF / Load PDF
 pdf_obj = st.file_uploader(
     "Carga tu documento / Upload your document", type="pdf")
 
-# Si no se ha cargado un PDF, no permitas que el usuario haga nada más
+# Si no se ha cargado un PDF, no permitas que el usuario haga nada más / If a PDF has not been uploaded, do not allow the user to do anything else
 if not pdf_obj:
     st.stop()
 
-# Función para crear embeddings
+# Función para crear embeddings / Function to create embeddings
 
 
 @st.cache_resource
 def create_embeddings(pdf):
-    # Leer PDF
+    # Leer PDF / Read PDF
     pdf_reader = PdfReader(pdf)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
 
-    # Dividir texto en fragmentos
+    # Dividir texto en fragmentos / Split text into fragments
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=100,
@@ -52,84 +51,51 @@ def create_embeddings(pdf):
     )
     chunks = text_splitter.split_text(text)
 
-    # Crear embeddings
+    # Crear embeddings / Create embeddings
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
     knowledge_base = FAISS.from_texts(chunks, embeddings)
 
     return knowledge_base, text
 
-# Función para generar resumen
 
-
-def generate_summary(text):
-    import openai
-
-    openai.api_key = API_KEY
-
-    # Cargar un tokenizador pre-entrenado
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    # Dividir el texto en tokens
-    tokens = tokenizer.tokenize(text)
-    num_tokens = len(tokens)
-    chunk_size = 4000
-    segments = [tokens[i:i + chunk_size]
-                for i in range(0, num_tokens, chunk_size)]
-    summary = ""
-
-    for segment in segments:
-        segment_text = tokenizer.convert_tokens_to_string(segment)
-
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Please provide a summary of the following text:\n\n{segment_text}" if lang ==
-                'en' else f"Por favor, proporciona un resumen del siguiente texto:\n\n{segment_text}"}
-        ]
-
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-                max_tokens=300,
-                temperature=0.5,
-            )
-        except openai.error.InvalidRequestError as e:
-            st.error(
-                "Ha ocurrido un error con la solicitud a la API de OpenAI. Por favor, inténtalo de nuevo más tarde.")
-            st.stop()
-
-        summary += response['choices'][0]['message']['content'].strip()
-
-    return summary
-
-
-# Principal
+# Principal / Main
 if pdf_obj:
-    # Crear embeddings
+    # Crear embeddings / Create embeddings
     knowledge_base, text = create_embeddings(pdf_obj)
 
-    # Detectar idioma
+    # Detectar idioma / Detect language
     lang = detect(text)
-    lang = 'en' if lang != 'es' else 'es'  # Solo considera inglés y español
+    # Solo considera inglés y español / Only considers English and Spanish
+    lang = 'en' if lang != 'es' else 'es'
 
-    # Opciones de usuario
+    # Seleccionar idioma / Select language
+    st.sidebar.header('Language' if lang == 'en' else 'Idioma')
+    lang = st.sidebar.radio(
+        "", ['English', 'Español'], index=0 if lang == 'en' else 1)
+    lang = 'en' if lang == 'English' else 'es'
+
+    # Opciones de usuario / User options
     st.sidebar.header('Options' if lang == 'en' else 'Opciones')
     options = [
         'Ask questions',
-        'Generate summary',
+        'Extract text',
+        'Summarize text',
+        'Translate text',
     ] if lang == 'en' else [
         'Realizar preguntas',
-        'Generar resumen',
+        'Extraer texto',
+        'Resumir texto',
+        'Traducir texto',
     ]
     selected_option = st.sidebar.selectbox(
         "What do you want to do with the PDF?" if lang == 'en' else "¿Qué deseas hacer con el PDF?", options)
 
-    # Preguntar
+    # Preguntar / Ask questions
     if selected_option == ('Ask questions' if lang == 'en' else 'Realizar preguntas'):
         st.header("Ask Questions" if lang == 'en' else "Realizar preguntas")
         user_question = st.text_input(
-            "Ask a question about your PDF:" if lang == 'en' else "Haz una pregunta sobre tu PDF:")
+            "Ask a question about your PDF" if lang == 'en' else "Haz una pregunta sobre tu PDF:")
 
         if user_question:
             os.environ["OPENAI_API_KEY"] = API_KEY
@@ -138,9 +104,14 @@ if pdf_obj:
             chain = load_qa_chain(llm, chain_type="stuff")
             respuesta = chain.run(input_documents=docs, question=user_question)
             st.write(respuesta)
+    else:
+        st.info("This option will be implemented soon" if lang ==
+                'en' else "Esta opción se implementará próximamente")
 
-    # Generar resumen
-    elif selected_option == ('Generate summary' if lang == 'en' else 'Generar resumen'):
-        st.header("Generate Summary" if lang == 'en' else "Generar resumen")
-        resumen = generate_summary(text, lang)
-        st.write(resumen)
+    # Footer / Pie de página
+    st.sidebar.markdown('---')
+    st.sidebar.subheader('Created by' if lang == 'en' else 'Creado por:')
+    st.sidebar.markdown('Alexander Oviedo Fadul')
+    st.sidebar.markdown(
+        "[GitHub](https://github.com/bladealex9848) | [Website](https://alexander.oviedo.isabellaea.com/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)"
+    )
